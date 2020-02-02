5850eb255195dee72b4bc9b7eeb320fa58f6b375
Bohdan Pohasii <bogdanpogasiy@gmail.com>
2020-02-02 10:56:01 +0200

    Add docker config for development

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
+console.log('MONGO', process.env.MONGO_DB_URL);
+
 mongoose.connect(process.env.MONGO_DB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-});
+})
+  .then(() => console.log('DB Connected!'))
+  .catch(err => {
+    console.log(Error, err.message);
+  });
