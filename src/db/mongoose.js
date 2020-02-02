c906b152b8d7bcc47f5487e4378af436bdc57549
Bohdan Pohasii <bogdanpogasiy@gmail.com>
2020-02-02 11:46:35 +0200

    Add docker for production

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
 
-console.log('MONGO', process.env.MONGO_DB_URL);
-
 mongoose.connect(process.env.MONGO_DB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-})
-  .then(() => console.log('DB Connected!'))
-  .catch(err => {
-    console.log(Error, err.message);
-  });
+});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on('open', console.error.bind(console, 'MongoDB connection success:'));
